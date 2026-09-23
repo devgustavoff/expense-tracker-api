ef874de57330c5c8e67e9098f16f7e4bf454c524
Gustavo Fernandes <fernandesferreiragustavo3@gmail.com>
2026-09-23 16:54:29 -0300

    added function to now how is the current user.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
-from sqlmodel import Session, SQLModel, create_engine, Field
-from datetime import datetime, timezone
+from sqlmodel import Session, SQLModel, create_engine, Field, select
+from datetime import datetime, timezone, timedelta
 from decimal import Decimal
 from sqlalchemy.exc import IntegrityError
 from pwdlib import PasswordHash
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+import jwt
+from jwt.exceptions import InvalidTokenError
+from typing import Annotated
+
+SECRET_KEY = "4081ba4c5e5fbdbdaaa415eb95bece4361aba25e2cb24b4c2adb3fa4f7f864a3"
+ALGORITHM = "HS256"
 
 def current_timestamp():
     return datetime.now(timezone.utc)
@@ -42,15 +49,32 @@ def get_session():
 
 password_hash = PasswordHash.recommended()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
 def verify_password(plain_password, hashed_password):
     return password_hash.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
     return password_hash.hash(password)
 
+def get_current_user(*, session: Session = Depends(get_session), token: str = Depends(oauth2_scheme)):
+    payload = decode_access_token(token=token)
+    statement = select(User).where(User.username == payload.get("sub"))
+    user = session.exec(statement=statement).first()
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not exists.")
+    return user
+
+def decode_access_token(token:str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Token invalid or expired")
+    return payload
+
 app = FastAPI()
 
-@app.post("/users", response_model=UserOut)
+@app.post("/singup", response_model=UserOut)
 async def create_user(*, session: Session = Depends(get_session), user: UserIn) -> User:
     new_user = User(username=user.username, password=get_password_hash(user.password))
     session.add(new_user)
@@ -58,6 +82,28 @@ async def create_user(*, session: Session = Depends(get_session), user: UserIn) 
         session.commit()
     except IntegrityError:
         session.rollback()
-        raise HTTPException(status_code=400, detail="Username already exists")
+        raise HTTPException(status_code=400, detail="Username already exists.")
     session.refresh(new_user)
     return new_user
+
+@app.post("/login")
+async def login(*, session: Session = Depends(get_session), form_data: OAuth2PasswordRequestForm = Depends()):
+    expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode = {
+        "sub": form_data.username,
+        "exp": expire
+        }
+    statement = select(User).where(User.username == form_data.username)
+    user_db = session.exec(statement=statement).first()
+    if user_db is None:
+        raise HTTPException(status_code=404, detail="Username not exists.")
+    if not verify_password(form_data.password, user_db.password):
+        raise HTTPException(status_code=401, detail="Password wrong")
+
+    access_token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.get("/me", response_model_exclude={"password"})
+async def me(current_user: User = Depends(get_current_user)) -> User:
+    return current_user
